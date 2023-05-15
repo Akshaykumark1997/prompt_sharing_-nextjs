d11a0c0fb4752486f04a3665b1deacd1ca05b237
Akshay kumar k <akshayajith40@gmail.com>
2023-05-15 17:55:20 +0530

    completed next auth api

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
 
 import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
   providers: [
@@ -12,12 +13,38 @@ const handler = NextAuth({
   ],
   async session({ session }) {
     try {
-      await connectToDB()
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
+
+      session.user.id = sessionUser._id.toString();
+
+      return session;
     } catch (error) {
-      
+      console.log(error);
     }
   },
-  async signIn({ profile }) {},
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+
+      //user exist
+
+      const userExist = await User.findOne({ email: profile.email });
+
+      //if user not exist 
+      
+      if(!userExist){
+        await User.create({
+          username: profile.name.replace(" ","").toLowerCase(),
+          email: profile.email,
+          image: profile.picture,
+        })
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
 });
 
 export {handler as GET , handler as POST};
