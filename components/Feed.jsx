78403f5894f33d6d3765b4f8a45307d0514da52e
Akshay <akshay@anjamerica.com>
2023-05-16 16:45:16 +0530

    completed prompt card

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+"use client";
+import { useState, useEffect } from "react";
+import PromptCards from "./PromptCards";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCards
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+  const handleSearchChange = (e) => {};
 
-export default Feed
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch("/api/prompts");
+      const data = await response.json();
+      setPosts(data);
+    };
+    fetchPost();
+  }, []);
+  return (
+    <section className="feed">
+      <form className="w-full flex-center relative">
+        <input
+          type="text"
+          placeholder="Search for a tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
